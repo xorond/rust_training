8e58af7c1a6817b2dda6dc192a6c3c1d277d0fb2
Oguz Bektas <o.bektas@proxmox.com>
2019-01-18 10:51:27 +0100

    fibonacci

--- chapter_3/practice/fibonacci/src/main.rs
+++ chapter_3/practice/fibonacci/src/main.rs
@@ -1,5 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("{}",fibonacci(5));
+    println!("Fibonacci(n) = ");
+
+    let mut step = String::new();
+
+    io::stdin().read_line(&mut step).expect("failed to read line");
+
+    let step: u32 = match step.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("expected an integer number");
+            return
+        }
+    };
+
+    println!("{}th Fibonacci: {}", step, fibonacci(step));
 }
 
 fn fibonacci(n: u32) -> u32 {
